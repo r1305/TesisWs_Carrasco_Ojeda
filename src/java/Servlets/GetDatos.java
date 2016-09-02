648db9a28d477b3884c5d5661401eff4aa486242
Julian <20101296@aloe.ulima.edu.pe>
2016-09-02 16:05:25 -0500

    afinidad v3

---
+++ src/java/Servlets/GetDatos.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import static com.mongodb.client.model.Filters.eq;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.bson.Document;
+import org.json.simple.JSONObject;
+
+/**
+ *
+ * @author Julian
+ */
+public class GetDatos extends HttpServlet {
+
+    private MongoClient client;
+    private final MongoClientURI clientURI = new MongoClientURI("mongodb://root:root@ds147995.mlab.com:47995/tesis_ojeda_carrasco");
+    private MongoDatabase database;
+    private MongoCollection<Document> col;
+    
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String id=request.getParameter("id");
+        String ob=getDatos(id);
+        response.setContentType("application/json");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.print(ob);
+            
+        }
+    }
+    
+    public void getConnection() {
+        client = new MongoClient(clientURI);
+        database = client.getDatabase("tesis_ojeda_carrasco");
+        col = database.getCollection("usuarios");
+    }
+
+    public String getDatos(String id) {
+        getConnection();
+        String data="";
+        try {
+            Document doc = col.find(eq("userFbId",id)).first();
+            JSONObject o=new JSONObject();
+            o.put("carrera", doc.getString("carrera"));
+            o.put("sexo",doc.getString("sexo"));
+            o.put("ciclo",doc.getInteger("ciclo"));
+            o.put("edad",doc.getInteger("edad"));
+            
+            data=o.toString();
+            
+        } catch (NullPointerException e) {
+            data="error";
+        }
+        return data;
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
